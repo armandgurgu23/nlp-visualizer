8593f0e703c50d36d187da977638d8e6b2171494
Armand Gurgu <armandgurgu23@gmail.com>
2021-05-08 23:22:30 -0400

    Config file for transformers models.

---
+++ src/configs/models/defaults/transformersDefaults.py
@@ -0,0 +1,23 @@
+# my_project/config.py
+from yacs.config import CfgNode as CN
+
+
+class TransformersConfigHandler(object):
+    def __init__(self):
+        self.initializeYTConfig()
+
+    def __call__(self, config_filepath):
+        return self.getTransformersConfig(config_filepath)
+
+    def getTransformersConfig(self, config_filepath):
+        defaults_config = self._transformers.clone()
+        defaults_config.merge_from_file(config_filepath)
+        defaults_config.freeze()
+        return defaults_config
+
+    def initializeYTConfig(self):
+        self._transformers = CN()
+        self._transformers.pipeline = CN()
+        self._transformers.pipeline.pipeline_type = "sentiment-analysis"
+        self._transformers.pipeline.pipeline_model = ""
+        return
